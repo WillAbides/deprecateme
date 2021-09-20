bedd0abc0761ea6596ebe398bbe9a7f7d4d6c619
Will Roden <will@roden.cc>
2021-09-20 15:22:39 -0500

    add some code

--- deprecateme.go
+++ deprecateme.go
@@ -1,1 +1,13 @@
 package deprecateme
+
+import (
+	"fmt"
+)
+
+// HelloWorld returns a greeting
+func HelloWorld(greetee string) string {
+	if greetee == "" {
+		greetee = "world"
+	}
+	return fmt.Sprintf("Hello, %s!", greetee)
+}
